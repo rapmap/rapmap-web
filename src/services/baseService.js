a4346dbf65de5eb0125f7cc2d392d5e8a7406039
Carlos Díaz Girol <carlosdiazgirol@MacBook-Pro-de-Carlos-2.local>
2019-03-30 11:42:25 +0100

    fit: CORS configuration

---
+++ src/services/baseService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: 'http://localhost:3001',
+  // withCredentials: true
+});
+
+http.interceptors.response.use(
+  response => response,
+  error => {
+    console.error(error);
+    if (error.status === 403) {
+      window.location = '/';
+    } else {
+      return Promise.reject(error);
+    }
+  }
+)
+
+
+export default http;
